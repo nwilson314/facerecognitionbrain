8d94385aec1118307d134783ac383df49f8722e3
Nate Wilson <nsw.wilson@gmail.com>
2021-06-11 13:30:44 -0400

    completed clarifai api integration and added simple signin/register routing

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -1,9 +1,18 @@
+import React, { useState } from 'react';
 import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '4fc957eb62964c569b6aaa84d4970661'
+});
 
 const particlesOptions = {
   particles: {
@@ -18,19 +27,78 @@ const particlesOptions = {
 }
 
 function App() {
+  const [textInput, setTextInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [box, setBox] = useState({});
+  const [route, setRoute] = useState('signin');
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  const displayFaceBox = (box) => {
+    setBox(box);
+  }
+
+  const onInputChange = (event) => {
+    setTextInput(event.target.value);
+  }
+
+  const onButtonSubmit = () => {
+    setImageUrl(textInput);
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, textInput).then(
+      response => {
+        displayFaceBox(calculateFaceLocation(response));
+      }
+    ).catch( err => console.log(err));
+  }
+
+  const onRouteChange = (rt) => {
+    if (rt === 'signin') {
+      setIsSignedIn(false);
+    } else if (rt === 'home') {
+      setIsSignedIn(true);
+    }
+    setRoute(rt)
+  }
+
   return (
     <div className="App">
       <Particles 
         className='particles'
         params={particlesOptions}
       />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      <br />
-      <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-    </div>
+      <Navigation onRouteChange={onRouteChange} isSignedIn={isSignedIn}/>
+      {
+        route === 'home' 
+        ? <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onButtonSubmit}/>
+            <FaceRecognition imageUrl={imageUrl} box={box}/>
+
+            <br />
+            <br />
+            <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+          </div>
+        : (
+          route === 'signin'
+          ? <Signin onRouteChange={onRouteChange}/>
+          : <Register onRouteChange={onRouteChange} />
+        )
+      }
+      </div>
     
   );
 }
